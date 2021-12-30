89f36f88267ed8b0f1544de31565936723353248
Jeong Ukjae <jeongukjae@gmail.com>
2021-12-30 13:41:46 +0900

    add hash index evaluator

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -168,6 +168,7 @@ func TestErrorHandling(t *testing.T) {
 			return 1;
 		}`, "unknown operator: BOOLEAN + BOOLEAN"},
 		{"foobar", "identifier not found: foobar"},
+		{`{"name": "Monkey"}[fn(x) {x}]`, "unhashable as hash key: FUNCTION"},
 	}
 
 	for _, tt := range tests {
@@ -339,6 +340,52 @@ func TestHashLiteral(t *testing.T) {
 		pair, ok := result.Pairs[key]
 		require.True(t, ok)
 		testIntegerObject(t, value, pair.Value)
+	}
+}
+
+func TestHashIndexExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected interface{}
+	}{
+		{
+			`{"foo": 5}["foo"]`,
+			5,
+		},
+		{
+			`{"foo": 5}["bar"]`,
+			nil,
+		},
+		{
+			`let key = "foo"; {"foo": 5}[key]`,
+			5,
+		},
+		{
+			`{}["bar"]`,
+			nil,
+		},
+		{
+			`{5:5}[5]`,
+			5,
+		},
+		{
+			`{true: 5}[true]`,
+			5,
+		},
+		{
+			`{false: 5}[false]`,
+			5,
+		},
+	}
+
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		integer, ok := tt.expected.(int)
+		if ok {
+			testIntegerObject(t, int64(integer), evaluated)
+		} else {
+			testNullObject(t, evaluated)
+		}
 	}
 }
 
